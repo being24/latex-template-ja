539a430520e08f6149e17ae3a95cd7c5d031da13
being <34680324+being24@users.noreply.github.com>
2022-05-20 00:26:57 +0900

    🎨 add files and text for latexdiff-vc

---
+++ diff.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 一つ前のコミットとこのコミットの差分
+latexdiff-vc -e utf8 -t CFONT --git --flatten --force -r HEAD main.tex
+
+# 現在と指定したIDの差分
+# latexdiff-vc -e utf8 -t CFONT --git --flatten --force -r commit-ID main.tex
+
+# 一つ前のタグと最新のタグの差分
+# git tag | sort -V | tail -n 2 | xargs -n 2 bash -c 'latexdiff-vc -e utf8 -t CFONT --git --flatten --force -r $0 -r $1 -t CFONT main.tex'

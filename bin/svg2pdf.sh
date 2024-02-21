f1b623027772c55572a1ce6fa03b1e32241c247a
being <34680324+being24@users.noreply.github.com>
2024-02-21 14:02:57 +0000

    空白を扱うためにコマンドを変更(sh→bash)

--- bin/svg2pdf.sh
+++ bin/svg2pdf.sh
@@ -3,8 +3,7 @@
 # convert svg to pdf using inkscape in figures directory
 
 cd /workdir
-FILES=`find ./ -name "*.svg"`
 
-for file in $FILES;do
-    inkscape --export-area-drawing --export-text-to-path $file --export-filename=${file%.*}.pdf
+find ./ -name "*.svg" -print0 | while IFS= read -r -d '' file; do
+    inkscape --export-area-drawing --export-text-to-path "$file" --export-filename="${file%.*}.pdf"
 done

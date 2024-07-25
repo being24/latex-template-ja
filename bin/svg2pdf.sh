f2f5c339029b299f8ae0431471a33f8afde3b611
being <34680324+being24@users.noreply.github.com>
2024-07-25 03:35:09 +0000

    chore: update svg2pdf.sh script to use xargs for better performance

--- bin/svg2pdf.sh
+++ bin/svg2pdf.sh
@@ -4,7 +4,15 @@
 
 cd /workdir
 
-find ./ -name "*.svg" -print0 | while IFS= read -r -d '' file; do
+# find ./ -name "*.svg" -print0 | while IFS= read -r -d $'\0' file; do
+#     new_file="${file// /_}"
+#     inkscape --export-area-drawing --export-text-to-path "$file" --export-filename="${new_file%.*}.pdf"
+# done
+
+# find ./ -name "*.svg" -print0 | xargs -0 -I {} echo "{}"
+
+find ./ -name "*.svg" -print0 | xargs -0 -I {} bash -c '
+    file="{}"
     new_file="${file// /_}"
     inkscape --export-area-drawing --export-text-to-path "$file" --export-filename="${new_file%.*}.pdf"
-done
+'

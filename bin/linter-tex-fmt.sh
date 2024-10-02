73324a73453e1dc4901496de6594fb95a2cf4678
being <34680324+being24@users.noreply.github.com>
2024-10-02 11:05:34 +0000

    Add linter-tex-fmt.sh script for formatting LaTeX files

---
+++ bin/linter-tex-fmt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /workdir
+FILES=$(find ./ -type f \( -name "*.tex" -o -name "*.bib" \) -not -name '__latexindent_temp.tex')
+# FILES=$(find ./ -type f \( -name "*.tex" -o -name "*.bib" -o -name "*.cls" -o -name "*.sty" \) -not -name '__latexindent_temp.tex')
+for file in $FILES;do
+    tex-fmt $file
+done
